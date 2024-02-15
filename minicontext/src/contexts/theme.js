f88fa8da574acaec769ce248ab8d01e11f19d346
Keelan Moore <135381527+HububPlaya@users.noreply.github.com>
2024-02-15 09:51:48 -0600

    finsihed with context

--- minicontext/src/contexts/theme.js
+++
@@ -1,9 +0,0 @@
-import { createContext, useContext } from "react";
-
-export const themeContext = createContext({
-    themeMode: "light",
-    darktheme: () => {},
-    lightTheme: () => {}
-
-    
-})
